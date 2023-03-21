0eb86faff068573c1a808281b29ab2d794ed10cb
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-03-21 12:37:50 -0400

    paths fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { Fragment } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import UnexpectedError from "./shared/components/UnexpectedError";
 import Unauthorized from "./shared/components/Unauthorized";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
-import CreateOrganization from "./dashboard/organizations/components/CreateOrganization";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import CreateOrganization from "./dashboard/pages/organizations/components/CreateOrganization";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element:
-      <CreateOrganization></CreateOrganization>,
+    path: "/",
+    element: <CreateOrganization></CreateOrganization>,
     errorElement: <UnexpectedError />,
   },
   {
@@ -20,12 +19,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/communications",
-
   },
   {
     path: "/Unauthorized",
     element: <Unauthorized></Unauthorized>,
-  }
+  },
 ]);
 
 function App() {
--- src/dashboard/pages/organizations/components/LoginForm.jsx
+++ src/dashboard/pages/organizations/components/LoginForm.jsx
@@ -1,62 +1,62 @@
 import { Fragment, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Joi from "joi";
-import Form from "../../../shared/components/Form";
-import accountService from "../../../services/accountService";
-import { getCurrentUser } from "../../../services/jwtService";
+import Form from "../../../../shared/components/Form";
+import accountService from "../../../../services/accountService";
+import { getCurrentUser } from "../../../../services/jwtService";
 
 const LoginForm = () => {
-	let navigate = useNavigate();
-	let location = useLocation();
-	const [data, setData] = useState({ email: "", password: "" });
-	const [errors, setErrors] = useState({});
+  let navigate = useNavigate();
+  let location = useLocation();
+  const [data, setData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({});
 
-	let from = location.state?.from?.pathname || "/";
+  let from = location.state?.from?.pathname || "/";
 
-	let currentUser = getCurrentUser();
-	if (currentUser?.userID) {
-		navigate(from, { replace: true });
-	}
+  let currentUser = getCurrentUser();
+  if (currentUser?.userID) {
+    navigate(from, { replace: true });
+  }
 
-	const schema = {
-		email: Joi.string().required().label("Email"),
-		password: Joi.string().required().label("Password"),
-	};
+  const schema = {
+    email: Joi.string().required().label("Email"),
+    password: Joi.string().required().label("Password"),
+  };
 
-	const doSubmit = async () => {
-		try {
-			const { email, password } = data;
-			await accountService.login(email, password);
+  const doSubmit = async () => {
+    try {
+      const { email, password } = data;
+      await accountService.login(email, password);
 
-			navigate(from, { replace: true });
-		} catch (ex) {
-			if (ex.response && ex.response.status === 401) {
-				const errorMsg = "Incorrect email or password.";
+      navigate(from, { replace: true });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 401) {
+        const errorMsg = "Incorrect email or password.";
 
-				const errorsClone = { ...errors };
-				errorsClone.email = errorMsg;
+        const errorsClone = { ...errors };
+        errorsClone.email = errorMsg;
 
-				setErrors(errorsClone);
-			}
-		}
-	};
+        setErrors(errorsClone);
+      }
+    }
+  };
 
-	let form = new Form(data, setData, errors, setErrors, schema, doSubmit);
-	return (
-		<Fragment>
-			<h1>Login</h1>
-			<form onSubmit={form.handleSubmit}>
-				{form.renderInput("email", "Email", "Enter your email address")}
-				{form.renderInput(
-					"password",
-					"Password",
-					"Enter your password",
-					"password"
-				)}
-				{form.renderButton("Login")}
-			</form>
-		</Fragment>
-	);
+  let form = new Form(data, setData, errors, setErrors, schema, doSubmit);
+  return (
+    <Fragment>
+      <h1>Login</h1>
+      <form onSubmit={form.handleSubmit}>
+        {form.renderInput("email", "Email", "Enter your email address")}
+        {form.renderInput(
+          "password",
+          "Password",
+          "Enter your password",
+          "password"
+        )}
+        {form.renderButton("Login")}
+      </form>
+    </Fragment>
+  );
 };
 
 export default LoginForm;
