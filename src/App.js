4e5e4a08922ab469513b2ce84a0f38f0c7d973e7
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-03-21 18:18:55 -0400

    changed routing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
 import { Fragment } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import UnexpectedError from "./shared/components/UnexpectedError";
 import Unauthorized from "./shared/components/Unauthorized";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
+import styles from "./App.module.css"; // Make sure to use .module.css
 import CreateOrganization from "./dashboard/pages/organizations/components/CreateOrganization";
 import Organizations from "./dashboard/pages/organizations/Organizations";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Organizations></Organizations>,
-    children: [
-      {
-        path: "/create-organization",
-        element: <CreateOrganization></CreateOrganization>,
-        errorElement: <UnexpectedError />,
-      },
-    ],
-  },
-  {
-    path: "/contacts",
-  },
-  {
-    path: "/communications",
-  },
-  {
-    path: "/Unauthorized",
-    element: <Unauthorized></Unauthorized>,
-  },
-]);
 
 function App() {
   return (
     <Fragment>
-      <ToastContainer></ToastContainer>
-      <RouterProvider router={router} />
+      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Organizations />} />
+          <Route path="/create-organization" element={<CreateOrganization />} />
+          <Route path="/contacts" />
+          <Route path="/communications" />
+          <Route path="/unauthorized" element={<Unauthorized />} />
+          <Route path="*" element={<UnexpectedError />} />
+        </Routes>
+      </Router>
     </Fragment>
   );
 }
