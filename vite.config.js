840721b9926f8d74158523c738571b0f7689ce3c
Ethan Lajeunesse <ethan@lajfam.com>
2023-03-21 20:49:03 -0400

    Update vite settings

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [reactRefresh()],
 	server: {
 		port: 3000,
+		host: true,
 	},
 });
