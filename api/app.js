1ca849f46e31aa6983a3ba9f9166f87215e8247f
Ethan Lajeunesse <ethan@lajfam.com>
2023-04-17 19:13:02 -0400

    Update CORS test

--- api/app.js
+++ api/app.js
@@ -24,18 +24,29 @@ database.sequelize
 		console.error("Unable to sync database:", err);
 	});
 
-const corsOptions = {
-	origin: "http://community-alb-1-1456052483.us-east-1.elb.amazonaws.com/",
-	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-	allowedHeaders: "Content-Type,Authorization",
-};
+var allowedOrigins = [
+	"http://localhost:3000",
+	"http://community-alb-1-1456052483.us-east-1.elb.amazonaws.com/",
+];
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			if (allowedOrigins.indexOf(origin) === -1) {
+				let msg =
+					"The CORS policy for this site does not " +
+					"allow access from the specified Origin.";
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
+	})
+);
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors(corsOptions));
 
 app.use("/", healthRouter);
 app.use("/contacts", contactsRouter);
