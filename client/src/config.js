f8f6968b639701d2a428e6a99371c8fa7346ced2
Ethan Lajeunesse <ethan@lajfam.com>
2023-04-17 23:15:30 -0400

    Update API url

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,6 @@
 export default {
-	apiUrl: "http://localhost:3001",
+	apiUrl:
+		process.env.NODE_ENV === "production"
+			? "http://community-alb-2-1638402382.us-east-1.elb.amazonaws.com:3001"
+			: "http://localhost:3001",
 };
