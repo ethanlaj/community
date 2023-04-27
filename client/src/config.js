ae9be8cf1def78146ec6b6a4e8708cc45a130af3
Ethan Lajeunesse <ethan@lajfam.com>
2023-04-27 18:47:32 -0400

    Update API urls

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export default {
 	apiUrl:
 		process.env.NODE_ENV === "production"
-			? "https://us-central1-community-9eef4.cloudfunctions.net/api/"
-			: "http://localhost:3001/community-9eef4/us-central1/api/",
+			? "https://community-production-c11b.up.railway.app"
+			: "http://localhost:3001",
 };
