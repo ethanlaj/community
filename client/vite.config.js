2c85f90881d37fde232bc96567dfe06b490f8224
Ethan Lajeunesse <ethan@lajfam.com>
2023-10-03 10:36:21 -0400

    Fix prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-	plugins: [reactRefresh()],
-	server: {
-		host: true,
-		port: 3000,
-	},
-	preview: {
-		host: true,
-		port: 3000,
-	},
-	resolve: {
-		alias: {
-			"@": "/src",
-		},
-	},
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
