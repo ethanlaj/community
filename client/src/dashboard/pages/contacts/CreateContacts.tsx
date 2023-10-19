a98dfab87a3dc0e651f8c63b8083ffb7cf03c8a5
BuckJ12 <cyanblanket@gmail.com>
2023-10-19 13:02:28 -0400

    Create Contacts Updated

--- client/src/dashboard/pages/contacts/CreateContacts.tsx
+++ client/src/dashboard/pages/contacts/CreateContacts.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import Joi from 'joi';
-import ContactService from '@/services/contactService'
+import ContactService from '@/services/contactService';
 import AddLocation from '@/dashboard/pages/organizations/AddLocation';
 import useForm from '@/shared/hooks/useForm';
 import AddOrganization from '@/dashboard/pages/organizations/AddOrganization';
@@ -15,7 +15,7 @@ interface CreateContactDTO {
 }
 
 interface FormProps {
- 
+
   organizations: {
     id: number;
     orgName: string;
@@ -39,14 +39,14 @@ function CreateContacts() {
   const nowLocal = new Date(now.getTime() - timeZoneOffset * 60 * 1000);
 
   const fields: FormProps = {
-      name: '',
-      email: '',
-      phone: '',
-      exten: '',
-      organizations: [],
-};
+    name: '',
+    email: '',
+    phone: '',
+    exten: '',
+    organizations: [],
+  };
 
-//  const [errors, setErrors] = useState({});
+  //  const [errors, setErrors] = useState({});
 
   const schema = Joi.object({
     name: Joi.string().required().label('Name'),
@@ -57,25 +57,25 @@ function CreateContacts() {
     phone: Joi.string().replace(/-/g, '').length(10).pattern(/^[0-9]+$/)
       // eslint-disable-next-line newline-per-chained-call
       .required().label('Phone'),
-      //exten: Joi.string().length(5).pattern(/^[0-9*#]+$/).label('Extension'),
+    // exten: Joi.string().length(5).pattern(/^[0-9*#]+$/).label('Extension'),
   });
 
   const doSubmit = async () => {
     try {
-      const locationIds = form.data.organizations ? form.data.organizations.map(location => location.id) : [];
-  
+      const locationIds = form.data.organizations ? form.data.organizations.map((location) => location.id) : [];
+
       const CreateContactDTO: CreateContactDTO = {
         name: form.data.name,
         email: form.data.email,
         phone: form.data.phone,
         exten: form.data.exten,
         locationIds,
-      }
-  
+      };
+
       console.log('Submit to api', { CreateContactDTO });
       console.log('ORG PASS DEFINATION');
       console.log(fields.organizations);
-  
+
       // Pass CreateContactDTO with the expected structure
       await ContactService.create(CreateContactDTO);
       navigate('/contacts', { replace: true });
@@ -83,7 +83,6 @@ function CreateContacts() {
       console.error(ex);
     }
   };
-  
 
   const form = useForm<FormProps>({ fields, schema, doSubmit });
 
@@ -91,9 +90,9 @@ function CreateContacts() {
     <div>
       <h1>Create Contacts</h1>
       <form className={`${styles.formContainer}`}>
-        {form.renderInput({id: 'name', label: 'Name'})}
-        {form.renderInput({id: 'email', label: 'Email'})}
-        {form.renderInput({id: 'phone', label: 'Phone Number'})}
+        {form.renderInput({ id: 'name', label: 'Name' })}
+        {form.renderInput({ id: 'email', label: 'Email' })}
+        {form.renderInput({ id: 'phone', label: 'Phone Number' })}
 
         <h3>Organization</h3>
         {form.renderChildForm(
@@ -101,14 +100,15 @@ function CreateContacts() {
           'organization',
           AddOrganization,
           form.data.organizations,
-          { organizationId: form.data.organizations })}
+          { organizationId: form.data.organizations },
+        )}
 
         {form.data.organizations && (
           <div>
             <h3>Location</h3>
-            {form.renderChildForm(form, 'location', AddLocation,form.data.organizations
-                ?.flatMap(organization => organization.locations?.map(location => location?.locName))
-                ?.filter(name => name !== null) || [], {
+            {form.renderChildForm(form, 'location', AddLocation, form.data.organizations
+              ?.flatMap((organization) => organization.locations?.map((location) => location?.locName))
+              ?.filter((name) => name !== null) || [], {
               organizationId: form.data.organizations,
             })}
           </div>
