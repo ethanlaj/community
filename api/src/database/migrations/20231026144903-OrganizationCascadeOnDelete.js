ea8d8a3db6b08b4806aa6d11dd673ff3474a5e66
MichaelPKennedy <111917176+MichaelPKennedy@users.noreply.github.com>
2023-10-26 18:40:42 -0400

    update migration script

--- api/src/database/migrations/20231026144903-OrganizationCascadeOnDelete.js
+++ api/src/database/migrations/20231026144903-OrganizationCascadeOnDelete.js
@@ -2,31 +2,31 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.sequelize.query(`
-      ALTER TABLE communications 
+	async up (queryInterface) {
+		await queryInterface.sequelize.query(`
+      ALTER TABLE Communications 
       DROP FOREIGN KEY communications_ibfk_1;
     `);
 
-    await queryInterface.sequelize.query(`
-      ALTER TABLE communications
+		await queryInterface.sequelize.query(`
+      ALTER TABLE Communications
       ADD CONSTRAINT communications_ibfk_1
       FOREIGN KEY (locationId) REFERENCES OrganizationLocations(id)
       ON DELETE CASCADE ON UPDATE CASCADE;
     `);
-  },
+	},
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.sequelize.query(`
-      ALTER TABLE communications 
+	async down (queryInterface) {
+		await queryInterface.sequelize.query(`
+      ALTER TABLE Communications 
       DROP FOREIGN KEY communications_ibfk_1;
     `);
 
-    await queryInterface.sequelize.query(`
-      ALTER TABLE communications
+		await queryInterface.sequelize.query(`
+      ALTER TABLE Communications
       ADD CONSTRAINT communications_ibfk_1
       FOREIGN KEY (locationId) REFERENCES OrganizationLocations(id)
       ON DELETE NO ACTION ON UPDATE CASCADE;
     `);
-  }
+	}
 };
