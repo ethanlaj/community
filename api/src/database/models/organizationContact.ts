72572efeaa29116bd4f6be165c31905aa9f5bd9f
BuckJ12 <cyanblanket@gmail.com>
2023-11-01 17:18:44 -0400

    fix double declaration

--- api/src/database/models/organizationContact.ts
+++ api/src/database/models/organizationContact.ts
@@ -25,7 +25,6 @@ export class OrganizationContacts extends Model {
 	@ForeignKey(() => Contacts)
 	@Column({ type: DataType.INTEGER, allowNull: false })
 		contactId!: number;
-	contactId!: number;
 
 	@PrimaryKey
 	@ForeignKey(() => Organizations)
