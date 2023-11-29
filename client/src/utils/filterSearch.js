2423d2eb570ee133e7d975b7414b9fe9b1d50cf9
BuckJ12 <cyanblanket@gmail.com>
2023-11-29 11:38:36 -0500

    Fixed Search to disallow Dates

--- client/src/utils/filterSearch.js
+++ client/src/utils/filterSearch.js
@@ -1,5 +1,8 @@
 const filterSearch = (objects, searchTerm, additionalValues = []) => objects.filter((obj) => {
-  const objValues = [...Object.values(obj), ...additionalValues];
+  // Exclude createdAt, updatedAt and id from the object values
+  const objValues = [...Object.entries(obj)
+    .filter(([key]) => key !== 'createdAt' && key !== 'updatedAt' && key !== 'id')
+    .map(([, value]) => value), ...additionalValues];
 
   if (objValues.some((value) => typeof value === 'string' && value.toLowerCase().includes(searchTerm.toLowerCase()))) {
     return true;
