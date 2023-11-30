1fc1bd3a01212fefb9725a1d54f88aaa2364db3a
BuckJ12 <cyanblanket@gmail.com>
2023-11-30 12:59:07 -0500

    Update Filter search to allow for ignoredKeys

--- client/src/utils/filterSearch.js
+++ client/src/utils/filterSearch.js
@@ -1,11 +1,25 @@
-const filterSearch = (objects, searchTerm, additionalValues = []) => {
+/*
+* objects is the orginal list of items
+* searchTerm is used to check against the item
+* additionalValues are any other values you may need to pass in
+* ignoredKeys allows you to define a group of keys you dont want to be checked from the search term
+*   Default ignoredKeys are createdAt updatedAt and id
+*/
+const filterSearch = (objects, searchTerm, additionalValues = [], ignoredKeys = []) => {
+  const fullignoredKeys = ['createdAt', 'updatedAt', 'id', ...ignoredKeys];
   const checkAliases = (obj) => {
     // Exclude createdAt, updatedAt and id from the object values
     const objValues = [...Object.entries(obj)
-      .filter(([key]) => key !== 'createdAt' && key !== 'updatedAt' && key !== 'id')
+      .filter(([key]) => !fullignoredKeys.includes(key))
       .map(([, value]) => value), ...additionalValues];
 
+    // Check for String matches
     if (objValues.some((value) => typeof value === 'string' && value.toLowerCase().includes(searchTerm.toLowerCase()))) {
+      return true;
+    }
+
+    // Check for number matches
+    if (objValues.some((value) => typeof value === 'number' && value === parseFloat(searchTerm))) {
       return true;
     }
 
